93aa7b9142ae34155092d4324b8f215e3e9f9bed
azdunczykowski <azdunczykowski@gmail.com>
2025-02-06 21:23:50 +0100

    Added snippets, pricing, testimonials, footer.

--- personal-finance-tracker/src/App.jsx
+++ personal-finance-tracker/src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Feature from "./components/Feature";
 import Snippets from "./components/Snippets";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
         <Hero />
         <Feature />
         <Snippets />
+        <Pricing />
+        <Testimonials />
+        <Footer />
       </div>
     </>
   );
